e8046986ea4a15fe7580c39ea94a512c98d6213f
LouisCR04 <louiskariuki04@gmail.com>
2023-06-15 23:24:39 +0300

    Task 6

---
+++ 0x00-hello_world/c_newTask.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Enter file name:"
+read file_nm
+echo -e "#include <stdio.h>\n/**\n  * main - Entry point\n  *\n  *Return: always 0\n  */\nint main(void)\n{\n\n	return (0);\n}"> $file_nm
+echo "File created"
+vim $file_nm
