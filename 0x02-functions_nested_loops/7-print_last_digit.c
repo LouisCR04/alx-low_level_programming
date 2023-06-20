682608894ffe37516d58c4e20198a232e51c0de9
LouisCR04 <louiskariuki04@gmail.com>
2023-06-20 22:02:45 +0300

    Took account of min integer which cant be expressed as an absolute int due to size.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,12 @@ int print_last_digit(int n)
 {
 	int z;
 
-	if (n < 0)
+	if (n < 0 && n != -2147483648)
 		z = (-n) % 10;
+	else if (n == -2147483648)
+		z = 8;
 	else
-	{
 		z = n % 10;
-	}
-	_putchar(z+'0');
+	_putchar(z + '0');
 	return (z);
 }
