b9924f51a72eca0028974803f48a40504578d24a
LouisCR04 <louiskariuki04@gmail.com>
2023-06-20 12:31:20 +0300

    Task 1 with header file main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *main - Entry point
  *Uses the _putchar fx created in the 2nd source code
  *to be linked
- *@c: Any ASCII character
  *
  *Return:Always 0(Success)
  */
 
-int _putchar(char c);
 
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef CMAINH
+#define CMAINH
+
+/**
+ * _putchar - writes the character c to std
+ * @c: Input ASCII character
+ *
+ * Return: A character
+ */
+char _putchar(char c);
+
+#endif
