db0c80c99aaf3df226138be1cff95cd28dfd58e7
LouisCR04 <louiskariuki04@gmail.com>
2023-06-21 15:16:07 +0300

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Fibonacci sequence
+ *
+ *Return:Always 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+	float n, m, sum;
+
+	n = 1;
+	m = 2;
+
+	printf("1, 2");
+	for (i = 1; i <= 48; i++)
+	{
+		sum = n + m;
+
+		printf(", %.0f", sum);
+		if ((i%2) == 0)
+		{
+			m = sum;
+		}
+		else if ((i%1) == 0)
+			n = sum;
+	}
+	printf("\n");
+	return (0);
+}
