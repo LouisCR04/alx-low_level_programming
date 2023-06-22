98d87716d2ab6aeb88870358281123f17a5b1bc9
LouisCR04 <louiskariuki04@gmail.com>
2023-06-22 16:30:09 +0300

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef CMAINN
+#define CMAINN
+
+/**
+ *positive_or_negative - Tests for positive or negative
+ *@i: An integer value
+ *
+ *Return: Void
+ */
+void positive_or_negative(int i);
+
+#endif
