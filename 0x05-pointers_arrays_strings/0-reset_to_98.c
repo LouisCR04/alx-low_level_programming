19e9d8b065a40b3d59c7ce62fc2d3adb24f22d0b
LouisCR04 <louiskariuki04@gmail.com>
2023-06-26 18:49:30 +0300

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Resets the value pointed by n to 98
+ *@n: A pointer to an integer value
+ *
+ *Return: Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _MMAIN_
+#define _MMAIN_
+
+/**
+ *_putchar - Prints a character to the std output
+ *@c: A character value
+ *
+ *Return: 1 if no error, -1 if error
+ */
+int _putchar(char c);
+
+/**
+ *reset_to_98 - Resets the value pointed by n to 98
+ *@n: A pointer to an integer value
+ *
+ *Return: Void
+ */
+void reset_to_98(int *n);
+
+#endif
