50b2bb3a79946ce9d174b61d88e148a7ab3af81b
LouisCR04 <louiskariuki04@gmail.com>
2023-06-27 01:21:11 +0300

    Limits error Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,16 @@
  */
 void print_array(int *a, int n)
 {
+	int *s = a;
 	int i;
-
-	for (i = 0; i < n - 2 ; i++)
+	
+	if (n < 0)
 	{
-		printf("%d, ", a[i]);
+		printf("\n");
 	}
-	printf("%d\n", a[n - 1]);
+	for (i = 0; i < n - 1 ; i++)
+	{
+		printf("%d, ", s[i]);
+	}
+	printf("%d\n", s[n - 1]);
 }
