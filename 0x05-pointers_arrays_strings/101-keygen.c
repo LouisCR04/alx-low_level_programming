d5f3755ef8c8327945a16f397cd21feb06ba43db
LouisCR04 <louiskariuki04@gmail.com>
2023-06-27 02:38:09 +0300

    Too tired

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return:Always 0(Success)
+ */
+
+int main(void)
+{
+	/**
+	 *Its 2:36 am and am too tired to even fathom 
+	 *the question. Maybe another day!
+	 */
+	return (0);
+}
