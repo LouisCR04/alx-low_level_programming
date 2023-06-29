e890b27f6dac0cd2107fc4c1ed30a1a8e5d373d1
LouisCR04 <louiskariuki04@gmail.com>
2023-06-29 16:19:18 +0300

    Task 6 corrections

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ *special - Checks for special characters
+ *@c: - Character
+ *
+ *Return: True(0) if special else false(0)
+ */
+int special(char c)
+{
+	int i = 0;
+
+	char array[] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(', ')', '{', '}'};
+
+	while (i < 13)
+	{
+		if (array[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
  *cap_string - changes all lowercase letters of a string to uppercase
  *@s: A pointer to an integer
  *
@@ -8,6 +29,18 @@
  */
 char *cap_string(char *s)
 {
+	int i = 0;
 
-	return (0);
+	while (s[i] != '\0')
+	{
+		if (special(s[i]))
+		{
+			i++;
+			continue;
+		}
+		if (s[i] >= 'a' && s[i] <= 'z' && (special(s[i - 1]) || i == 0))
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
 }
