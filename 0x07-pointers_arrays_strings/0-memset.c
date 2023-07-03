85a16742ec3c265c71874f58431789e2371e1977
LouisCR04 <louiskariuki04@gmail.com>
2023-07-03 17:48:22 +0300

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset - Fills memory buffer with specified xter
+ *@s: A pointer to a buffer
+ *@b: Character used to replace
+ *@n: Bytes to fill
+ *
+ *Return: Pointer to the buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef CMMAIN
+#define CMMAIN
+
+/**
+ *_memset - Fills memory buffer with specified xter
+ *@s: A pointer to a buffer
+ *@b: Character used to replace
+ *@n: Bytes to fill
+ *
+ *Return: Pointer to the buffer
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
