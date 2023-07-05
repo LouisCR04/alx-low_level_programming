af00eff2423e28a1e4c4f2ca35b4fab9ccf654e3
LouisCR04 <louiskariuki04@gmail.com>
2023-07-05 19:16:41 +0300

    Task 1 Betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ *_print_rev_recursion - Prints a reverse string using
+ *recursion
+ *@s: Pointer to the string
+ *
+ *Return: void
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
