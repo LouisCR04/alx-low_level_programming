5fcdf40cba7a54517fd01e2cc217ae080b09c572
LouisCR04 <louiskariuki04@gmail.com>
2023-07-05 23:10:24 +0300

    Task 5 Squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *fnsqrt - Finds the root of an integer
+ *@n: An integer value
+ *@root: Acts like a counter towards the root
+ *
+ * Return: The root
+ */
+int fnsqrt(int n, int root)
+{
+	if (root == n / 2)
+		return (-1);
+	else if ((root *root) == n)
+		return (root);
+	else
+		return (fnsqrt(n, root + 1));
+}
+
+/**
+ *_sqrt_recursion - Acts as a wrapper for Finding the
+ *squaroot of an integer
+ *@n: An integer value
+ *
+ * Return: Squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 2;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (fnsqrt(n, root));
+}
