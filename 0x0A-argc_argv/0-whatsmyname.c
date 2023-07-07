3f7ea13596473113878ca6553adbf7227609466c
LouisCR04 <louiskariuki04@gmail.com>
2023-07-07 14:11:59 +0300

    Task 0 unused argc voided

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,15 @@
 
 /**
  *main - Entry point
+ *@argc: Arguments count
+ *@argv: Pointer to arrays containing CL arguments
  *
  *Return:Always 0(Success)
  */
 
 int main(int argc, char **argv)
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
-        return (0);
+	return (0);
 }
--- 0x0A-argc_argv/template.c
+++ 0x0A-argc_argv/template.c
@@ -2,11 +2,13 @@
 
 /**
  *main - Entry point
+ *@argc: Arguments count
+ *@argv: Pointer to arrays containing CL arguments
  *
  *Return:Always 0(Success)
  */
 
-int main(void)
+int main(int argc, char **argv)
 {
-        return (0);
+	return (0);
 }
