5bb357cec7691058da16f1cc4ddb8623b87a0ff4
LouisCR04 <louiskariuki04@gmail.com>
2023-07-11 22:14:20 +0300

    Advanced Extra Points, Should look into later

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+# include <stdlib.h>
+
+/**
+ *argstostr - concatenates all the arguments of your program
+ *@ac: An integer value
+ *@av: A pointer to a pointer
+ *
+ * Return: A pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	return (*av);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *strtow - splits a string into words
+ *@str: A pointer to a string
+ *
+ * Return: A pointer to a pointer
+ */
+
+char **strtow(char *str)
+{
+	char **ptr = &str;
+
+	return (ptr);
+}
