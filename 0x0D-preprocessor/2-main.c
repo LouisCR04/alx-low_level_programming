7604e3008cb520c2f8efcb2f96eef6b6e175f57e
LouisCR04 <louiskariuki04@gmail.com>
2023-07-17 23:40:58 +0300

    Task 2: Current file name macro (__FILE__)

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entrance
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/c_newTask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Enter file name:"
-read file_nm
-cp template.h $file_nm
-echo "File created"
-vim $file_nm
