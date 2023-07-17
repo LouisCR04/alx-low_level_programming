8c69aa957621c8710d576dbf1b4fb6954ae91b04
LouisCR04 <louiskariuki04@gmail.com>
2023-07-18 01:45:17 +0300

    Task 4 Betty compliance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- *new_dog - Creates a new dog struct
- *@str: Pointer to a string
+ *_strlen - Calculates length of a string excluding the null
+ *@str: PPointer to a string
  *
  * Return: Length of string
  */
@@ -19,7 +19,7 @@ int _strlen(char *str)
 /**
  *_strcpy - Copies string from src to dest
  *@dest: Pointer to a string
- *@str: Pointer to a string
+ *@src: Pointer to a string
  *
  * Return: Pointer to copied string
  */
@@ -27,10 +27,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for(i = 0; i < _strlen(src); i++)
+	for (i = 0; i < _strlen(src); i++)
 	{
 		dest[i] = src[i];
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
@@ -46,6 +47,7 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
@@ -54,19 +56,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
+
 	ptr->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
 	ptr->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-        if (ptr->owner == NULL)
-        {
-                free(ptr);
-                free(ptr->name);
-                return (NULL);
-        }
+	if (ptr->owner == NULL)
+	{
+		free(ptr);
+		free(ptr->name);
+		return (NULL);
+	}
+
 	ptr->name = _strcpy(ptr->name, name);
 	ptr->owner = _strcpy(ptr->owner, owner);
 	ptr->age = age;
