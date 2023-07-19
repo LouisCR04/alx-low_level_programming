6ccd5507dbb10f36d7979eed36243f0b4f941d5a
LouisCR04 <louiskariuki04@gmail.com>
2023-07-20 01:56:09 +0300

    Task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <udis86.h>
+
+/**
+ *main - entry
+ *@argc: No of arguments from terminal
+ *@argv: Arguments from terminal
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	ud_t ud_obj;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ud_init(&ud_obj);
+	ud_set_input_buffer(&ud_obj, argv[1], atoi(argv[1]));
+	ud_set_mode(&ud_obj, 64);
+	ud_setsyntax(&ud_obj, UD_SYN_INTEL);
+
+	while (ud_disassemble(&ud_obj))
+		printf("\t%s\n", ud_insn_hex(&ud_obj));
+	return (0);
+}
--- 0x0F-function_pointers/c_newTask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Enter file name:"
-read file_nm
-cp template_c.c $file_nm
-echo "File created"
-vim $file_nm
