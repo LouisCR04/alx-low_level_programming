da6bcec99eb8640765594ac1bf3f4674070630ec
LouisCR04 <louiskariuki04@gmail.com>
2023-07-20 16:35:06 -0700

    Task 2: Printing no:s Variadic fxs

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - prints numbers separated by a comma or whatever
+ *@separator: separates the numbers being printed
+ *@n: No. of args passed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	if (n > 0)
+	{
+		va_start(args, n);
+
+		while (i < n)
+		{
+			printf("%d", va_arg(args, int));
+			if (i != n - 1 && separator != NULL)
+				printf("%s", separator);
+			i++;
+		}
+	}
+	printf("\n");
+}
