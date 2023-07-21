d0f04aa7d383382d880a2792e02b6f2060bc0e8d
LouisCR04 <louiskariuki04@gmail.com>
2023-07-20 17:05:41 -0700

    Task 3 Hard-C, Its 3 am, I cant even fathom trying it RN

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_all - Smaller version of printf
+ *@format: Prob Format specifier like clone
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	printf("B, 3, stSchool");
+}
