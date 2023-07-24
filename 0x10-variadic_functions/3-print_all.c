36c2b948e42a370eda7259912ff14ff208fafc73
LouisCR04 <louiskariuki04@gmail.com>
2023-07-24 11:31:08 +0300

    Edits

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,14 +1,44 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 
 /**
- *print_all - Smaller version of printf
- *@format: Prob Format specifier like clone
+ *Prints Almost anything to the std output
+ *@format: list of types of arguments passed to the function 
  *
  * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
-	printf("B, 3, stSchool");
+	va_list args;
+	f_spec f_types[] = {
+		{"c", char_print},
+		{"i", int_print},
+		{"f", float_print},
+		{"s", str_print}
+	};
+
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
+
+	va_start(args, format);
+
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *f_types[j].identifier)
+			{
+				f_types[j].f(separator, args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n);
+}
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,4 +20,10 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+typedef struct form_spec
+{
+	char *specifier;
+	void (*f)(char *separator, va_list args);
+}f_spec;
+
 #endif /*_VARFX_*/
