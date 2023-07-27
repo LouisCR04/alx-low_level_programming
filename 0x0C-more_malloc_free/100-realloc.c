3876c2c85a6a59c3c9d52889495b34f437bc45fd
LouisCR04 <louiskariuki04@gmail.com>
2023-07-27 23:50:47 +0300

    Task 4: Realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_realloc - Reallocated buffer
+ *@ptr: Pointer to buffer to reallocate
+ *@old_size: Size of ptr
+ *@new_size: Size of new buffer
+ *
+ * Return: pointers or void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new;
+	unsigned int i = 0;
+
+	if (new_size < old_size)
+		return (NULL);
+	if (ptr == NULL)
+	{
+		new = malloc(new_size);
+		if (new == NULL)
+			return (NULL);
+		return (new);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	while (i < old_size && i < new_size)
+	{
+		new[i] = ((char *)ptr)[i];
+		i++;
+	}
+
+	free (ptr);
+	return (new);
+}
