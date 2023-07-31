8f19e1d083910d62a1b224aa26055d32b88f532f
LouisCR04 <louiskariuki04@gmail.com>
2023-07-31 16:09:13 +0300

    Task 9: Inserting a node at index n in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ins_node;
+	listint_t *ins_node, *traverser;
+	unsigned int i;
 
-	return (0);
+	if (*head == NULL)
+		return (NULL);
+
+	ins_node = malloc(sizeof(listint_t));
+	if (ins_node == NULL)
+		return (NULL);
+
+	ins_node->n = n;
+
+	if (n == 0)
+	{
+		ins_node->next = *head;
+		*head = ins_node;
+		return (ins_node);
+	}
+
+	traverser = *head;
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		traverser = traverser->next;
+
+		if (traverser == NULL)
+			return (NULL);
+	}
+
+	ins_node->next = traverser->next;
+	traverser->next = ins_node;
+
+	return (ins_node);
 }
