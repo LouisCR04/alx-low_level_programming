5c89d66d7b315147dc65bb13967ed9ed56994831
LouisCR04 <louiskariuki04@gmail.com>
2023-07-31 15:36:43 +0300

    Debugged Task 7- Double increament in for loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 		if (head == NULL)
 			return (NULL);
-		i++;
 	}
 	return (head);
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - Inserts a new node at a given index
+ *of a linked list
+ *@head: Head to a linked list
+ *@idx: Index where a new node is to be inserted
+ *@n: An integer value
+ *
+ * Return: Address of the inserted node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ins_node;
+
+	return (0);
+}
