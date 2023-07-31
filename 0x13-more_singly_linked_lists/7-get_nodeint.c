e3b8089a3b8ee3cc59d061094fe418aade699bbb
LouisCR04 <louiskariuki04@gmail.com>
2023-07-31 15:10:54 +0300

    Task 7: Getting an indexed element

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - Gets a node at given index
+ *@head: Head to linked list
+ *@index: Node to get
+ *
+ * Return: The indexed node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+
+		if (head == NULL)
+			return (NULL);
+		i++;
+	}
+	return (head);
+}
