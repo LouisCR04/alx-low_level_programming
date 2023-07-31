a03e31c1ccd936793443acc8718a14ed797e5e47
LouisCR04 <louiskariuki04@gmail.com>
2023-07-31 16:32:38 +0300

    Fixed a for loop issue in task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	traverser = *head;
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index - 1; i++)
 	{
 		traverser = traverser->next;
 
