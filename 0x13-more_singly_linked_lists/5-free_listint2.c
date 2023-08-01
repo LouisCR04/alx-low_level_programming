ff1af28ffcde8b81145882a3f68ea429621a2b9c
LouisCR04 <louiskariuki04@gmail.com>
2023-08-01 10:41:12 +0300

    Tsk 5: where head is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *traverser;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head != NULL)
@@ -20,6 +20,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(traverser);
 	}
-	free(*head);
-	free(*head);
+	head = NULL;
 }
