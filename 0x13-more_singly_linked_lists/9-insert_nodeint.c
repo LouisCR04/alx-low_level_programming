32b58d9aa05c446f60604ecf0498942d680d56d3
LouisCR04 <louiskariuki04@gmail.com>
2023-08-01 10:57:38 +0300

    Betty compliance

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ins_node, *traverser;
 	unsigned int i;
 
-	if(head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	ins_node = malloc(sizeof(listint_t));
