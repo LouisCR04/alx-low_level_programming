d6d1a674992b12c9de92f596a27decc3378cbafb
LouisCR04 <louiskariuki04@gmail.com>
2023-08-03 14:41:43 +0300

    Betty Compliance

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,13 +3,14 @@
 /**
  *get_bit - Gets bit at given index
  *@n: Integer value to get bit from
+ *@index: Given index
  *
  * Return: value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(n)*8))
-			return (-1);
+	if (index >= (sizeof(n) * 8))
+		return (-1);
 
 	if ((n & (1 << index)) != 0)
 		return (1);
