7cd0902c49b791b05f2250014baf0cad29f9e307
LouisCR04 <louiskariuki04@gmail.com>
2023-08-08 00:44:19 +0300

    Task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+/**
+ *main - Entry point
+ *@argc: Argument Count
+ *@argv: Arguments
+ *@__attribute__: voids
+ *
+ * Return: Always 0
+ */
+int main(int __attribute__((__unused__)) argc,
+		char __attribute__((__unused__)) *argv[])
+{
+	return (0);
+}
