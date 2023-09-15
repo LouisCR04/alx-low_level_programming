65e3642f93c05ff81708e7ad3fe192250b10713f
LouisCR04 <louiskariuki04@gmail.com>
2023-09-15 11:51:01 +0300

    Added block if there is an existing list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,13 +11,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+
+	if (*head != NULL)
+	{
+		new->next = *head;
+		new->prev = (*head)->prev;
+		(*head)->prev = new;
+		*head = new;
+		return (new);
+	}
+
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
